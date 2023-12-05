a7055278ba50043b686df5162d6445c97fe8d7d7
devlumba <ilovesylphy@gmail.com>
2023-12-05 22:50:59 +0300

    added virtual environment, nothing big

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
